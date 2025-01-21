cec0c280aa716f18e91217f75774dfa8adb5b249
jloliveira95 <jl.oliveira95@gmail.com>
2025-01-20 21:59:40 -0300

    Criando Relacionamento de Tabelas

--- blogpessoal/src/main/java/com/generation/blogpessoal/controller/PostagemController.java
+++ blogpessoal/src/main/java/com/generation/blogpessoal/controller/PostagemController.java
@@ -22,6 +22,7 @@ import org.springframework.web.server.ResponseStatusException;
 
 import com.generation.blogpessoal.model. Postagem;
 import com. generation. blogpessoal. repository . PostagemRepository;
+import com.generation.blogpessoal.repository.TemaRepository;
 
 import jakarta.validation.Valid;
 
@@ -33,6 +34,9 @@ public class PostagemController {
 
 	@Autowired											// O Spring dá autonomia para a interface poder invocar os métodos
 	private PostagemRepository postagemRepository;
+	
+	@Autowired
+	private TemaRepository temaRepository; 
 		
 	@GetMapping 										// indica que este método é chamado em Verbos/Métodos HTTP do tipo Get
 	public ResponseEntity<List<Postagem>> getAll() {
@@ -53,16 +57,25 @@ public class PostagemController {
 	
 	@PostMapping
 	public ResponseEntity<Postagem> post(@Valid @RequestBody Postagem postagem) {
-	    return ResponseEntity.status(HttpStatus.CREATED)
+	    if (temaRepository.existsById(postagem.getTema().getId()))
+	        return ResponseEntity.status(HttpStatus.CREATED)
 	            .body(postagemRepository.save(postagem));
+
+	    throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Tema não existe!", null);
 	}
-	
+
 	@PutMapping
 	public ResponseEntity<Postagem> put(@Valid @RequestBody Postagem postagem) {
-	    return postagemRepository.findById(postagem.getId())
-	        .map(resposta -> ResponseEntity.status(HttpStatus.OK)
-	        .body(postagemRepository.save(postagem)))
-	        .orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+	    if (postagemRepository.existsById(postagem.getId())) {
+
+	        if (temaRepository.existsById(postagem.getTema().getId()))
+	            return ResponseEntity.status(HttpStatus.OK)
+	                .body(postagemRepository.save(postagem));
+
+	        throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Tema não existe!", null);
+	    }
+
+	    return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
 	}
 
 	
--- blogpessoal/src/main/java/com/generation/blogpessoal/model/Postagem.java
+++ blogpessoal/src/main/java/com/generation/blogpessoal/model/Postagem.java
@@ -2,10 +2,14 @@ package com.generation.blogpessoal.model;
 
 import java.time.LocalDateTime;
 import org.hibernate.annotations.UpdateTimestamp;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
@@ -30,6 +34,17 @@ public class Postagem {
 	@UpdateTimestamp
 	private LocalDateTime data; // 10/10/25 10:33
 	
+	@ManyToOne
+	@JsonIgnoreProperties("postagem")
+	private Tema tema;
+	
+	
+	public Tema getTema() {
+		return tema;
+	}
+	public void setTema(Tema tema) {
+		this.tema = tema;
+	}
 	public Long getId() {
 		return id;
 	}
